3214618269b057c5fea9cf7eb61d8c68d5622f72
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-06-23 15:50:35 -0300

    Logica: Mapa de Contacto

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,18 @@
 const d = document;
 const menuBtn = d.querySelector('nav .btn');
 const menuLst = d.querySelector('nav .menu');
+const mapBtn = d.querySelector('#footer .btn[class*="icon"]');
+const mapFrm = d.querySelector('#footer .map')
+
 
 // Navegacion
 menuBtn.onclick = () => { 
     menuLst.classList.toggle('active')
 }
+
+// Mapa de contacto
+mapBtn.addEventListener('click', () => {
+    mapFrm.classList.toggle('active') ? 
+    mapBtn.classList.replace('icon-max', 'icon-min') :
+    mapBtn.classList.replace('icon-min', 'icon-max') 
+})
